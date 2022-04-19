4c547c7936df367d80ba93bd97269abc4cf2fac1
F3XS <103441440+F3XS@users.noreply.github.com>
2022-04-20 03:42:18 +0800

    Update Loader.lua

--- Main/Loader.lua
+++ Main/Loader.lua
@@ -1,2 +1,5 @@
 -- Field Goal Simulator
 loadstring(game:HttpGet("https://raw.githubusercontent.com/F3XS/Field-Goal-Simulator/main/Main/Script.lua"))()
+
+-- Tower Of Hell
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/Tower-Of-Hell/main/Main/Script.lua"))()
