fae42ad8d8b37404145fc764a5d1bb97b7a3e0f8
F3XS <103441440+F3XS@users.noreply.github.com>
2022-04-21 02:31:18 +0800

    Update Loader.lua

--- Main/Loader.lua
+++ Main/Loader.lua
@@ -1,5 +1,13 @@
 -- Field Goal Simulator
+if game.PlaceId == 9030362964 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/F3XS/Field-Goal-Simulator/main/Main/Script.lua"))()
 
 -- Tower Of Hell
+elseif game.PlaceId == 1962086868 or 3582763398 then
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/Tower-Of-Hell/main/Main/Script.lua"))()
+
+-- 3008 [2.7]
+elseif game.PlaceId == 2768379856 then
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/3008--2.7-/main/Main/Script.lua"))()
+  
+end
