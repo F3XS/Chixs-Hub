b6923948dff177ef087ec6d641c244620de921c3
Chixs <103441440+F3XS@users.noreply.github.com>
2022-04-23 00:40:43 +0800

    Update Loader.lua

--- Main/Loader.lua
+++ Main/Loader.lua
@@ -1,11 +1,26 @@
 -- Field Goal Simulator
+if game.PlaceId == 9030362964 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/F3XS/Field-Goal-Simulator/main/Main/Script.lua"))()
+end
 
 -- Tower Of Hell
+if game.PlaceId == 1962086868 then
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/Tower-Of-Hell/main/Main/Script.lua"))()
+elseif game.PlaceId == 3582763398 then
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/Tower-Of-Hell/main/Main/Script.lua"))()
+end
 
 -- 3008 [2.7]
+if game.PlaceId == 370731277 then
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/3008/main/Main/Script.lua"))()
+end
 
 -- Anarchy 
+if game.PlaceId == 370731277 then
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/Anarchy/main/Main/Script.lua"))()
+end
+
+-- MeepCity
+if game.PlaceId == 370731277 then
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/F3XS/MeepCity/main/Main/Script.lua"))()
+end
